a423541f46f6ac4890aa3cb58de0c2b23c048156
LaravelDevBose <laravel.devbose@github.com>
2018-03-10 23:15:12 +0600

    Error Handeling
    some small error was handel

--- resources/views/backEnd/paymentSystem/editPaymentContent.blade.php
+++ resources/views/backEnd/paymentSystem/editPaymentContent.blade.php
@@ -58,26 +58,30 @@ Payment System-Update
 
                             <div class="col-md-6">
                                 <div class="form-group">
-                                    <label>Ammount: <span class="text-danger">*</span> (.Tk) </label>
-                                    <input type="text" name="amount" class="form-control required " value="{{ $paymentSystemById->amount }}" >
-                                    <p class="text text-info"> This Field Ammount Will Use For Per day/week/mounth Payment !</p>
+                                    <label>Time Duration: <span class="text-danger text-bold">*</span></label>
+                                    <input type="number" name="duration" class="form-control required " value="{{ $paymentSystemById->duration }}" required placeholder="Enter The Duration of Working Time">
+                                    <p class="text text-info"> Enter The Deuration of Working Time</p>
                                 </div>
                             </div>
                         </div>
 
                         <div class="row">
-                            <div class="col-md-6">
+                            <div class="col-md-6"> 
                                 <div class="form-group">
-                                    <label>Over Time Ammount: <span class="text-danger">*</span> </label>
-                                    <input type="text" name="overTimeAmmount" class="form-control required " value="{{ $paymentSystemById->overTimeAmmount }}" >
-                                    <p class="text text-info">Over Time Ammount Will Count For Per Hour Work !</p>
+                                    <label>The Unit: <span class="text-danger text-bold">*</span></label>
+                                    <select class="select" name="unit"  required autofocus >
+                                            <option value="1">Hour </option>
+                                            <option value="2">Day</option>
+                                            <option value="3">Month</option>
+                                    </select>
+                                    <p class="text text-info">Select The working Time Duration Unit !</p>
                                 </div>
                             </div>
 
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label>Publicatiopn Status: <span class="text-danger">*</span></label>
-                                    <select class="select" name="publicationStatus" value="{{ old('publicationStatus') }}" required autofocus >
+                                    <select class="select" name="status"  required autofocus >
                                         <optgroup label="Select Publication Type">
                                             <option value="1">Publish </option>
                                             <option value="0">UnPublish</option>
@@ -101,7 +105,8 @@ Payment System-Update
 
 <!-- //Depertment Information Insert -->
   <script>
-    document.forms['editPaymentForm'].elements['publicationStatus'].value={{ $paymentSystemById->publicationStatus }}
+    document.forms['editPaymentForm'].elements['status'].value={{ $paymentSystemById->status }}
+    document.forms['editPaymentForm'].elements['unit'].value={{ $paymentSystemById->unit }}
 </script>
 
 
